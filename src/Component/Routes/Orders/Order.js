0a95a502b97d1fd6e183c90d7bc27b5eedc56c26
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-24 14:52:52 +0500

    add-data-in-new-product-page

--- src/Component/Routes/Orders/Order.js
+++ src/Component/Routes/Orders/Order.js
@@ -2,7 +2,39 @@ import React from 'react'
 import "./style.css"
 const Order = () => {
   return (
-    <div>Order</div>
+    <div className='container'>
+      <h3 className='text-center m-4'><strong>Orders page</strong></h3>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
   )
 }
 
