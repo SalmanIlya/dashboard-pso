879a1ce7402e4217b2c3fb38acd559377752441c
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-24 14:46:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import NewProduct from './Component/Routes/NewProduct/NewProducts'
 import Order from './Component/Routes/Orders/Order'
 import Expense from './Component/Routes/Expense/Expense'
 import Setting from './Component/Routes/Setting/Setting'
+import DeleteProducts from './Component/Routes/DeleteProducts/DeleteProducts'
+import PSO from "./Component/Routes/PSO/PSO"
+import NewExpance from './Component/Routes/New Expense/NewExpance'
 const App = () => {
   return (
     <div className='bg-light'>
@@ -17,12 +20,14 @@ const App = () => {
       <SideNav/>
       <main className='bg-light'>
         <Routes>
-          <Route exect path='/' element={<Dashboard/>} />
+          <Route exect path='/dashboard' element={<Dashboard/>} />
           <Route exect path='/products' element={<Products/>} />
           <Route exect path='/new/product' element={<NewProduct/>} />
           <Route exect path='/orders' element={<Order/>} />
-          <Route exect path='/delete/products' element={<Dashboard/>} />
+          <Route exect path='/delete/products' element={<DeleteProducts/>} />
+          <Route exect path='/pso' element={<PSO/>} />
           <Route exect path='/expense' element={<Expense/>} />
+          <Route exect path='/new/expense' element={<NewExpance/>} />
           <Route exect path='/setting' element={<Setting/>} />
         </Routes>
       </main>
